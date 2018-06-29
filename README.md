e7f4ba9dbf5a50413955099b1f0ce81ab439de13
xiaolu289 <312351283@qq.com>
2018-06-29 18:10:20 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# swiper2.fade
+# swiper2.fade.plugin
 基于swiper2实现的fade切换插件
 开发背景：
 因为swiper3并不兼容IE8，而swiper2能够兼容IE8，但是在swiper下，当时我并没有找到有实现fade效果的插件，因此，就自己动手造了一个，使用非常简单：
@@ -20,4 +20,18 @@ var mySwiper = new Swiper('.swiper-container',{
         swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
     }
 });  
+```
+
+# swiper2.animate.plugin
+
+为了简化使用animate的配置。。我将其代码直接移入到插件里面。。
+代码来源于swiper.animate : http://www.swiper.com.cn/usage/animate/
+
+
+```javascript
+var mySwiper = new Swiper('.swiper-container',{   
+    fade: true,
+    // loop: true,
+    animate:true, // 修改于swiper.animate插件，因此不支持loop模式
+}); 
 ```
---
+++ swiper.animate.plugin/swiper.animate.plugin.js
@@ -0,0 +1,75 @@
+﻿//本插件由www.swiper.com.cn提供
+//版本1.03
+Swiper.prototype.plugins.animate = function(swiper,params) {
+    var activedDOM = null;
+    function swiperAnimateCache(a)
+    {
+        for (j = 0; j < a.slides.length; j++)
+        {
+            for (allBoxes = a.slides[j].querySelectorAll(".ani"), i = 0; i < allBoxes.length; i++)
+            {
+                if (allBoxes[i].attributes["style"]) {
+                    allBoxes[i].setAttribute("swiper-animate-style-cache", allBoxes[i].attributes["style"].value)
+                } else {
+                    allBoxes[i].setAttribute("swiper-animate-style-cache", " ");
+                    allBoxes[i].style.visibility = "hidden";
+                }
+            }
+        }
+    }
+    function swiperAnimate(a)
+    {
+        clearSwiperAnimate(a);
+        var b = a.slides[a.activeIndex].querySelectorAll(".ani");
+        for (i = 0; i < b.length; i++)
+        {
+            b[i].style.visibility = "visible";
+            effect = "";
+            if (b[i].attributes["swiper-animate-effect"]) {
+                effect = b[i].attributes["swiper-animate-effect"].value;
+            }
+            b[i].className = b[i].className + "  " + effect + " " + "animated";
+            style = b[i].attributes["style"].value;
+            duration = "";
+            if (b[i].attributes["swiper-animate-duration"]) {
+                duration = b[i].attributes["swiper-animate-duration"].value;
+            }
+            if (duration) {
+                style = style + "animation-duration:" + duration + ";-webkit-animation-duration:" + duration + ";";
+            }
+            delay = b[i].attributes["swiper-animate-delay"] ? b[i].attributes["swiper-animate-delay"].value : "";
+            if (delay) {
+                style = style + "animation-delay:" + delay + ";-webkit-animation-delay:" + delay + ";";
+            }
+            b[i].setAttribute("style", style);
+        }
+    }
+    function clearSwiperAnimate(a)
+    {
+        for (j = 0; j < a.slides.length; j++)
+        {
+            for (allBoxes = a.slides[j].querySelectorAll(".ani"), i = 0; i < allBoxes.length; i++)
+            {
+                allBoxes[i].attributes["swiper-animate-style-cache"] && allBoxes[i].setAttribute("style", 
+                allBoxes[i].attributes["swiper-animate-style-cache"].value), allBoxes[i].style.visibility = "hidden", 
+                allBoxes[i].className = allBoxes[i].className.replace("animated", " "), allBoxes[i].attributes["swiper-animate-effect"] && (effect = allBoxes[i].attributes["swiper-animate-effect"].value, 
+                allBoxes[i].className = allBoxes[i].className.replace(effect, " "));
+            }
+        }
+    }
+
+    var hooks = {
+        onFirstInit: function(){ //Swiper2.x的初始化是onFirstInit
+            swiperAnimateCache(swiper); //隐藏动画元素 
+            swiperAnimate(swiper); //初始化完成开始动画
+            activedDOM = swiper.activeSlide();
+        }, 
+        onSlideChangeStart: function(){ 
+            swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+            activedDOM = swiper.activeSlide();
+        }
+    }
+
+    return hooks;
+}
+
